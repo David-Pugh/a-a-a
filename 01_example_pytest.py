eff00bb1d14cdde6a56265ea3b44acb59314deb6
David Pugh <35071038+David-Pugh@users.noreply.github.com>
2019-11-11 16:33:43 +0000

    Update 01_example_pytest.py

--- 01_example_pytest.py
+++ 01_example_pytest.py
@@ -6,23 +6,20 @@ Created on Mon Nov 11 09:38:59 2019
 @author: pughd
 """
 
-
 # Create a class to hold our tests
+# These should be testing the edge cases
 class TestClass:
 
-    def test_one(self):
-        assert small_function(2,5) == 7
+    def test_eq_100(self):
+        assert small_function(100,5) == 100
     
-    def test_two(self):
-        assert small_function(2,6) == 85
+    def test_gtr_100(self):
+        assert small_function(102,6) == 100
         
-    def test_three(self):
-        assert small_function(2,89) == 85
+    def test_lt_100(self):
+        assert small_function(2,89) == 91
         
-    def test_four(self):
-        assert small_function(235,89) == 100
-        
-        
+               
 # Define our function
 def small_function(x,y):
     if x< 100:
